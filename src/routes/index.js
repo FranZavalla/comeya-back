cfab82fb650424f48519b0bece1a1aa1efc9bf56
FranZavalla <franzavalla85@gmail.com>
2022-01-31 19:08:53 -0300

    App working

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,7 @@ module.exports = (app) => {
   const indexRouter = Router();
 
   indexRouter.get("/", (req, res) => {
-    res
-      .header("Access-Control-Allow-Origin", "*")
-      .send({ status: `Welcome to ComeYa API` });
+    res.send({ status: `Welcome to ComeYa API` });
   });
 
   return indexRouter;
